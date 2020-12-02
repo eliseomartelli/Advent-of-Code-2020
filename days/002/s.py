fe9c94559a15d463579c7ebb5bc1978d0a86aaeb
Eliseo Martelli <martely98@gmail.com>
2020-12-02 21:56:29 +0100

    fix: minimize character s1

--- days/002/s.py
+++ days/002/s.py
@@ -3,7 +3,7 @@ import re
 I = re.findall("([0-9]+)-([0-9]+) ([a-zA-Z]): ([a-z]+)", open("input").read())
 
 # Part 1
-s = sum([1 for l in I if int(l[0]) <= l[3].count(l[2]) <= int(l[1])])
+s = sum([int(l[0]) <= l[3].count(l[2]) <= int(l[1]) for l in I])
 
 print(s)
 
