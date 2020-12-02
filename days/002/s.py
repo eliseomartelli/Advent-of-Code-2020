58bd47b3b8518bac4f02a569922531b59fd8d5fe
Eliseo Martelli <me@eliseomartelli.it>
2020-12-02 19:14:02 +0100

    feat: add part 2 solution

--- days/002/s.py
+++ days/002/s.py
@@ -5,3 +5,8 @@ I = re.findall("([0-9]+)-([0-9]+) ([a-zA-Z]): ([a-z]+)", open("input").read())
 s = sum([1 for l in I if int(l[0]) <= l[3].count(l[2]) <= int(l[1])])
 
 print(s)
+
+
+s = sum([(l[3][int(l[0]) - 1] == l[2]) ^ (l[3][int(l[1]) - 1] == l[2]) for l in I])
+
+print(s)
