911826719d750b409fb2619bc1f7cc1659ce258d
Eliseo Martelli <me@eliseomartelli.it>
2020-12-03 19:06:34 +0100

    feat: add part 2 solution

--- days/003/s.py
+++ days/003/s.py
@@ -1,6 +1,12 @@
+from math import prod
+
 I = [l.strip() for l in open("input").readlines()]
 
 # Part 1
 s = sum([l[i * 3 % len(l)] == "#" for i, l in enumerate(I)])
+
+print(s)
+
+s = prod([sum([l[i * a[0] % len(l)] == "#" for i, l in enumerate(I[0 :: a[1]])]) for a in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]])
 
 print(s)
